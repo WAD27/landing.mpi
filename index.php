69a72894b971de826e810801e3cd83b5f1852ffa
josecaos <jazzvoon@gmail.com>
2019-07-08 23:06:01 -0500

    V1.1.1 fixes

--- index.php
+++ index.php
@@ -12,82 +12,94 @@
   <!--  -->
 </head>
 <body>
+  <div id="main"class="main container">
 
-  <section id="heroscreen" class="container-fluid no-padding">
-
-
-    <!-- <div class="background-video col-12 no-padding"> -->
-
-    <!-- <video
-    class="fullscreen-bg__video"
-    src="assets/videos/mpi.background.ogv"
-    type="video/ogv"
-    poster="mpi.background.png"
-    autoplay="true"
-    loop="true">
-    <source src="assets/videos/mpi.background.webm" type="video/webm"/>
-    <source src="assets/videos/mpi.background.mp4" type="video/mp4"/>
-  </video> -->
+    <section id="heroscreen" class="container-fluid no-padding">
 
 
-  <!-- </div> -->
-  <div class="row">
-    <center class="heroscreen-background col-12 ha my-auto">      
-      <!-- <div class="top col-12"></div> -->
-      <figure>
-        <img src="assets/img/mpi.m.png" alt="Meeting Planners International Logo">
-      </figure>
-    </center>
-  </div>
-  <div class="title col-12 ha">
+      <!-- <div class="background-video col-12 no-padding"> -->
+
+      <!-- <video
+      class="fullscreen-bg__video"
+      src="assets/videos/mpi.background.ogv"
+      type="video/ogv"
+      poster="mpi.background.png"
+      autoplay="true"
+      loop="true">
+      <source src="assets/videos/mpi.background.webm" type="video/webm"/>
+      <source src="assets/videos/mpi.background.mp4" type="video/mp4"/>
+    </video> -->
+
+
+    <!-- </div> -->
     <div class="row">
-      <h2 class="title-left col-12 col-md-6">MEETING</h2>
-      <h2 class="title-right col-12 col-md-6">PLANNERS</h2>
-      <p class="col-12 text-center">Somos una empresa especializada en la planeación y organización profesional.</p>
-      <!-- <p class="col-12 text-center">Nuestro sitio está en desarrollo, pronto tendrás más noticias de nosotros.</p> -->
-      <p class="col-12 text-center">
-        <span class="servicio-1">Viajes de Incentivo</span>
-        <span class="servicio-2">Convenciones</span>
-        <span class="servicio-1">Congresos</span>
-        <span class="servicio-2">Exposiciones</span>
-        <span class="servicio-1">Eventos Corporativos</span>
-      </p>
+
+      <center class="heroscreen-background col-12 ha my-auto">
+        <!-- <div class="top col-12"></div> -->
+        <figure class="col-12">
+          <img src="assets/img/mpi.m.png" alt="Meeting Planners International Logo">
+        </figure>
+        <div class="title col-12 ha">
+          <div class="row">
+            <h2 class="title-left col-12 col-md-6">MEETING</h2>
+            <h2 class="title-right col-12 col-md-6">PLANNERS</h2>
+            <p class="col-12 text-center">Somos una empresa especializada en la planeación y organización profesional.</p>
+            <!-- <p class="col-12 text-center">Nuestro sitio está en desarrollo, pronto tendrás más noticias de nosotros.</p> -->
+            <p class="col-12 text-center">
+              <span class="servicio-1">Viajes de Incentivo</span>
+              <span class="servicio-2">Convenciones</span>
+              <span class="servicio-1">Congresos</span>
+              <span class="servicio-2">Exposiciones</span>
+              <span class="servicio-1">Eventos Corporativos</span>
+            </p>
+
+          </div>
+
+        </div>
+      </center>
 
     </div>
 
-  </div>
+  </section>
 
-</section>
-
-<!--  -->
-<footer class="container-fluid">
-  <div class="row">
-    <div class="footer col-xs-12 col-md-6">
+  <!--  -->
+  <footer class="container-fluid">
+    <div class="row">
+      <div class="footer col-12 col-sm-6">
+        <div class="row">
+          <div class="col-12 ha my-auto">
+            <!-- <p class="col-12 text-center">
+            Copyright 2019
+          </p> -->
+          <p class="col-12 text-center">
+            Contáctanos: <a href="mailto:carlos.sandoval@mpi.mx">carlos.sandoval@mpi.mx</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="footer col-12 col-sm-6">
       <div class="row">
         <div class="col-12 ha my-auto">
-          <!-- <p class="col-12 text-center">
-          Copyright 2019
-        </p> -->
-        <p class="col-12 text-center">
-          Contáctanos: <a href="mailto:carlos.sandoval@mpi.mx">carlos.sandoval@mpi.mx</a>
-        </p>
+          <p class="col-12 text-center no-padding">
+            Oficina:<a class="col-12" href="tel:525552810040">+52 55 52810040</a>
+            Celular:<a class="col-12" href="tel:525551024242">+52 1 55 51024242</a>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="footer col-xs-12 col-md-6">
-    <div class="row">
-      <div class="col-12 ha my-auto">
-        <p class="col-12 text-center">
-          Oficina:<a class="col-12" href="tel:525552810040">+52 55 52810040</a>
-          Celular:<a class="col-12" href="tel:525551024242">+52 1 55 51024242</a>
-        </p>
-      </div>
-    </div>
-  </div>
 
-</div>
+    <div class="bottom-footer col-12">
+      <p class="col-12 text-center">
+        <small>
+          Copyright | MPI México 2019
+        </small>
+      </p>
+    </div>
+
+  </div>
 </footer>
 
+</div>
 <script src="js/app.js"></script>
 </body>
 </html>
